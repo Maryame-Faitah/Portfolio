28b63290cd1d27ee14747793fe2a80ffb32778ef
Maryame <faitah.maryame@gmail.com>
2020-02-20 12:09:32 +0100

    commit3-section1Final

--- src/section1.jsx
+++ src/section1.jsx
@@ -7,7 +7,7 @@ function Section1() {
                 <div className="row no-gutters">
                     <div className="col-md-6">
                         {/* <img src="./img/imgSect1.jpg" className="card-img" alt="..." /> */}
-                        <iframe width="450" height="500" src="https://www.youtube.com/embed/fOJ-KKtzDXI" 
+                        <iframe width="450" height="500" src="https://www.youtube.com/embed/fOJ-KKtzDXI"
                             frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
                         </iframe>
                     </div>
@@ -33,9 +33,10 @@ function Section1() {
                                 <div className="progress-bar w-82" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">82%</div>
                             </div>
                             <h6 className="font-weight-bold">Marketing</h6>
-                            <div className="progress">
+                            <div className="progress mb-3">
                                 <div className="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
                             </div>
+                            <a className="btn btn-sm font-weight-bold text-white mt-4"href="./img/imgSect1.jpg" download="Maryame_CV">Download my CV</a>
                         </div>
                     </div>
                 </div>
